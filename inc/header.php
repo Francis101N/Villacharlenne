9f028235b0c5688561fa58d4507542e1924c2fbd
Francis <francis@Franciss-MacBook-Pro.local>
2026-03-13 08:19:47 +0100

    first commit

---
+++ inc/header.php
@@ -0,0 +1,48 @@
+ <header class="header-area">
+        <!-- Navbar Area -->
+        <div class="palatin-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="palatinNav">
+
+                        <!-- Nav brand -->
+                        <a href="index.html" class="nav-brand"><img src="img/core-img/logo.png" alt=""></a>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="" style="background-color: blue;"><a href="index.html" class="text-light">Home</a></li>
+                                    <li><a href="/VillaCharlenne/about-us">About Us</a></li>
+                                    <li><a href="services.php">Services</a></li>
+                                    <li><a href="rooms.php">Rooms</a></li>
+                                    <li><a href="">News</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+
+                                <!-- Button -->
+                                <div class="menu-btn">
+                                    <a href="#" class="btn p-3" style="background-color:blue;">Make a Reservation</a>
+                                </div>
+
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
